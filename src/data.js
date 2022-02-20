08121689b5d86613511fb3e4690ef8ba4bfec736
Adam Kimball <adamkimball@Adams-MacBook-Pro.local>
2022-02-20 11:10:35 -0700

    Update styling and layout of project section

--- src/data.js
+++ src/data.js
@@ -15,17 +15,35 @@ export const projects = [
     image: "./verisys.gif",
     link: "https://verisys.com",
   },
+  {
+    title: "Whistle Recruiting",
+    subtitle: "React Native, React, Ruby on Rails, and PostgresQL",
+    description:
+      "Whistle Recruiting helps connect collegiate athletes and coaches. I worked on Whistle during its initial stages and focused on the React Native design, and building out API endpoints for both mobile and web apps.",
+    image: "./whistle.gif",
+    link: "http://www.whistlerecruiting.com/",
+  },
+  {
+    title: "Cell LinQ",
+    subtitle: "React Native and NodeJs",
+    description:
+      "Cell LinQ helps cellular technicians and installers perform cellular data collection. I built out simple React Native design and set up map integration with data points.",
+    image: "./CellLinQ.png",
+    link: "https://apps.apple.com/us/app/cell-linq/id1435242173?platform=iphone",
+  },
 ];
 
 export const skills = [
   "JavaScript",
   "CSS",
   "HTML",
-  "React",
-  "Node",
+  "React/React Hooks",
+  "React Native",
+  "NodeJs/Express",
   "Ruby on Rails",
   "PostgresQL",
   "MySQL",
   "Jest/Cypress",
   "Rspec",
+  "Git/Linux CLI",
 ];
